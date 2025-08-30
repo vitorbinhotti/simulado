99bb92c6f2a1e26242a4cedbea4a283ea0d6fa9b
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-08-30 09:57:42 -0300

    update - arrumando create

---
+++ banco-de-dados/schema.sql
@@ -0,0 +1,34 @@
+create database crud_SAEP;
+use crud_SAEP;
+
+create table turmas (
+    id_turma int not null auto_increment primary key,
+    nome varchar(45) not null,
+    serie varchar(45) not null,
+    quantidade_aluno int not null default (0)
+)
+
+create table professores (
+    id_professor int not null auto_increment primary key,
+    nome varchar(45) not null,
+    email varchar(85) not null,
+    materia varchar(45) not null,
+    fk_turma int not null,
+    FOREIGN KEY (fk_turma) references turmas(id_turma)
+)
+
+create table alunos (
+    id_aluno int not null auto_increment primary key,
+    nome varchar(45) not null,
+    fk_turma int not null,
+    FOREIGN KEY (fk_turma) references turmas(id_turma)
+)
+
+create table atividades (
+    id_atividade int not null auto_increment primary key,
+    descricao varchar(45) not null,
+    fk_turma int not null,
+    FOREIGN KEY (fk_turma) references turmas(id_turma),
+    fk_professor int not null,
+    FOREIGN key (fk_professor) references professores(id_professor)
+)
---
+++ create.php
@@ -0,0 +1,144 @@
+<?php
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['addTurma'])) {
+        $nome = $_POST['nome'];
+        $serie = $_POST['serie'];
+
+        $sql = "INSERT INTO turmas(nome, serie) VALUES ('$nome', '$serie')";
+
+        if ($conn->query($sql) == true) {
+            echo "Registro Adicionado";
+            $conn->close();
+            exit();
+        } else {
+            echo "Erro" . $sql . "<br>" . $conn->error;
+            $conn->close();
+        };
+    };
+
+    if (isset($_POST['addProfessor'])) {
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $materia = $_POST['materia'];
+        $fk_turma = $_POST['fk_turma'];
+
+        $sql = "INSERT INTO professores(nome, email, materia, fk_turma) VALUES ('$nome', '$email', '$materia', '$fk_turma')";
+
+        if ($conn->query($sql) == true) {
+            echo "Registro Adicionado";
+            $conn->close();
+            exit();
+        } else {
+            echo "Erro" . $sql . "<br>" . $conn->error;
+            $conn->close();
+        };
+    };
+
+    if (isset($_POST['addAluno'])) {
+        $nome = $_POST['nome'];
+        $fk_turma = $_POST['fk_turma'];
+
+        $sql = "INSERT INTO alunos(nome, fk_turma) VALUES ('$nome', '$fk_turma')";
+
+        if ($conn->query($sql) == true) {
+            echo "Registro Adicionado";
+            $conn->close();
+            exit();
+        } else {
+            echo "Erro" . $sql . "<br>" . $conn->error;
+            $conn->close();
+        };
+    };
+
+    if (isset($_POST['addAtividade'])) {
+        $descricao = $_POST['descricao'];
+        $fk_turma = $_POST['fk_turma'];
+        $fk_professor = $_POST['fk_professor'];
+
+        $sql = "INSERT INTO alunos(descricao, fk_turma, fk_professor) VALUES ('$descricao', '$fk_turma', '$fk_professor')";
+
+        if ($conn->query($sql) == true) {
+            echo "Registro Adicionado";
+            $conn->close();
+            exit();
+        } else {
+            echo "Erro" . $sql . "<br>" . $conn->error;
+            $conn->close();
+        };
+    };
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Registro</title>
+</head>
+
+<body>
+    <form method="POST">
+        <label for="">Adicionar Turma:</label>
+        <br><br>
+        <input type="text" name="nome" placeholder="Digite o Nome da Turma">
+        <br><br>
+        <input type="text" name="serie" placeholder="Digite a Série da Turma">
+        <br><br>
+        <button type="submit" name="addTurma">Adicionar Turma</button>
+    </form>
+
+    <br><br>
+
+    <form method="POST">
+        <label for="">Adicionar Professor:</label>
+        <br><br>
+        <input type="text" name="nome" placeholder="Digite o Nome do Professor">
+        <br><br>
+        <input type="email" name="email" placeholder="Digite o email">
+        <br><br>
+        <input type="text" name="fk_turma" placeholder="ID turma">
+        <br><br>
+        <select name="materia">
+            <option>Selecione uma materia</option>
+            <option value="ciencias">Ciências</option>
+            <option value="historia">História</option>
+            <option value="portugues">Português</option>
+            <option value="matematica">Matemática</option>
+        </select>
+        <br><br>
+        <button type="submit" name="addProfessor">Adicionar Turma</button>
+    </form>
+
+    <br>
+
+    <form method="POST">
+        <label for="">Adicionar Aluno:</label>
+        <br><br>
+        <input type="text" name="nome" placeholder="Digite o Nome do Aluno">
+        <br><br>
+        <input type="text" name="fk_turma" placeholder="Digite a Turma">
+        <br><br>
+        <button type="submit" name="addAluno">Adicionar Turma</button>
+    </form>
+
+    <br>
+
+    <form method="POST">
+        <label for="">Adicionar Atividade:</label>
+        <br><br>
+        <input type="text" name="nome" placeholder="Digite a descrição">
+        <br><br>
+        <input type="text" name="fk_turma" placeholder="Digite a turma">
+        <br><br>
+        <input type="text" name="fk_professor" placeholder="Digite o professor">
+        <br><br>
+        <button type="submit" name="addAtividade">Adicionar Turma</button>
+    </form>
+</body>
+
+</html>
